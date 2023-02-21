0059911ccd5494f29b248381e87963827bb6bdf7
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-02-21 20:52:05 +0300

    feat: backend login page start

--- backend/account/views.py
+++ backend/account/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.permissions import AllowAny
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,ChangePasswordSerializer,UpdateUserSerializer
 from rest_framework import generics
-
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+    
+class UpdateProfileView(generics.UpdateAPIView):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
